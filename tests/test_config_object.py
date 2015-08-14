746daf478d3d753fdcda85f7158a56c716f39cde
Eric Larson <eric.larson@rackspace.com>
2015-08-14 14:31:03 +0000

    Allow scripts as config

--- tests/test_config_object.py
+++ tests/test_config_object.py
@@ -11,11 +11,18 @@ class TestYAMLConfig(object):
         assert c()[0]['foo'] == 'bar'
 
 
+class TestScriptConfig(object):
+
+    def test_load_config_file(self):
+        c = config.ScriptConfig(os.path.join(HERE, 'etc', 'spanky', 'script'))
+        script = c()
+        assert script.run
+
+
 class TestConfigObject(object):
 
     def setup(self):
         self.config = config.Config(os.path.join(HERE, 'etc', 'spanky'))
 
     def test_load_config_file(self):
-        conf = self.config.load('example.yml')
-        assert conf[0]['foo'] == 'bar'
+        conf = self.config.load('script')
