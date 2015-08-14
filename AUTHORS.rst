2274052ce0150549e303a8c520f3fffd49ade388
Tim Simmons <tim.r.simmons@gmail.com>
2015-08-13 20:24:33 -0500

    Add Tim Simmons as contributor

--- AUTHORS.rst
+++ AUTHORS.rst
@@ -10,4 +10,4 @@ Development Lead
 Contributors
 ------------
 
-None yet. Why not be the first?
+* Tim Simmons <tim.r.simmons@gmail.com>
--- spanky/__init__.py
+++ spanky/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__author__ = 'Eric Larson'
-__email__ = 'eric@ionrock.org'
+__author__ = 'Eric Larson, Tim Simmons'
+__email__ = 'eric@ionrock.org, tim.r.simmmons@gmail.com'
 __version__ = '0.1.0'
