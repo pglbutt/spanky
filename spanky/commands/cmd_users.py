664c0924ca3a5ba58d0205c2fd733e6ad2e4f5dd
Eric Larson <eric.larson@rackspace.com>
2015-08-14 16:08:04 +0000

    Use the config when creating users.

--- spanky/commands/cmd_users.py
+++ spanky/commands/cmd_users.py
@@ -1,8 +1,11 @@
 import click
 from spanky.cli import pass_context
+from spanky.lib.users import UserInit
 
 
 @click.command('users', short_help='creates users base on /etc/spanky/users')
 @pass_context
 def cli(ctx):
-    config = ctx.config.load('users.yml')
+    config = ctx.config.load('users.yml')()
+    user_init = UserInit(config)
+    user_init.build()
