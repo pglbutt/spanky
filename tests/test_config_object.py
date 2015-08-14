7ae9f1cefb01a48e4fabf6109953c88a141f19a5
Eric Larson <eric.larson@rackspace.com>
2015-08-14 14:24:40 +0000

    Basic yml file loading for config

--- tests/test_config_object.py
+++ tests/test_config_object.py
@@ -1,14 +1,21 @@
 import os
 
-from spanky.config import Config
+from spanky import config
 
 HERE = os.path.dirname(os.path.abspath(__file__))
+
+class TestYAMLConfig(object):
+
+    def test_load_config_file(self):
+        c = config.YAMLConfig(os.path.join(HERE, 'etc', 'spanky', 'example.yml'))
+        assert c()[0]['foo'] == 'bar'
 
 
 class TestConfigObject(object):
 
     def setup(self):
-        self.config = Config(HERE)
+        self.config = config.Config(os.path.join(HERE, 'etc', 'spanky'))
 
-    def test_config_is_like_dict(self):
-        assert self.config.get('example')
+    def test_load_config_file(self):
+        conf = self.config.load('example.yml')
+        assert conf[0]['foo'] == 'bar'
