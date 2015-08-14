fbb5f07d77ee2d0833eb707cc7f1ca3f262b2560
Eric Larson <eric.larson@rackspace.com>
2015-08-14 18:51:43 +0000

    Enrollment working

--- spanky/lib/enroll.py
+++ spanky/lib/enroll.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 import redis
 
 
@@ -9,19 +11,53 @@ class Enroller(object):
     @property
     def conn(self):
         if not hasattr(self, '_conn'):
-            self._conn = redis.StrictRedis(host='localhost', port=6379, db=0)
+            self._conn = redis.StrictRedis(
+                host=self.config.get('host', 'localhost'),
+                port=int(self.config.get('port', 6379)),
+                db=0
+            )
         return self._conn
 
+    def value(self, host, port):
+        return '%s:%s' % (host, port)
+
+    def clear(self, name):
+        self.conn.delete(name)
+
     def join(self, name, host, port):
-        self.conn.lpush(name, '%s:%s' % (host, port))
+        self.conn.sadd(name, self.value(host, port))
 
     def enrolled(self, name):
-        return self.conn.lrange(name, 0, -1)
+        return self.conn.smembers(name)
+
+    def leave(self, name, host, port):
+        self.conn.srem(name, self.value(host, port))
+
+
+
+
+class enroll(object):
+
+    def __init__(self, config, name, host, port):
+        self.enroller = Enroller(config)
+        self.name = name
+        self.host = host
+        self.port = port
+        self.args = (self.name, self.host, self.port)
+
+    def __enter__(self):
+        self.enroller.join(*self.args)
+
+    def __exit__(self, *args):
+        print('leaving: %s %s %s' % self.args)
+        self.enroller.leave(*self.args)
 
 
 def main():
     e = Enroller({})
+    e.clear('foo')
     e.join('foo', 'bar', 8080)
+    e.join('foo', 'bar', 9090)
     print(e.enrolled('foo'))
 
 if __name__ == '__main__':
